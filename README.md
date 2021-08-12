f826df831537df42f32c2049c819d50bc08d63dc
noortitan <titan.hartono@gmail.com>
2021-08-12 13:35:57 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,68 @@
+# Dissimilarity Matrix for Perovskite Capping-Absorber Pairs
+This documentation is prepared as the workflow to accompany the following study:
+
+**"Tailoring capping-layer composition for improved stability of mixed-halide perovskites"**
+
+Noor Titan Putri Hartono (1), Marie-Hélène Tremblay (2), Sarah Wieghold (3), Benjia Dou (1), Janak Thapa (1), Armi Tiihonen (1), Vladimir Bulovic (1), Lea Nienhaus (4), Seth R. Marder (2,5-8), Shijing Sun (1), Tonio Buonassisi (1)
+
+Affiliations:
+
+1. Massachusetts Institute of Technology, 77 Massachusetts Avenue, Cambridge, MA 02139
+2. Georgia Institute of Technology, North Avenue, Atlanta, GA 30332
+3. Argonne National Laboratory, 9700 S. Cass Avenue, Lemont, IL 60439
+4. Florida State University, 600 W College Ave, Tallahassee, FL 32306
+5. University of Colorado Boulder, Renewable and Sustainable Energy Institute, Boulder, CO 80303
+6. University of Colorado Boulder, Department of Chemical and Biological Engineering, Boulder, CO 80303
+7. University of Colorado Boulder, Department of Chemistry, Boulder, CO 80303
+8. National Renewable Energy Laboratory, Chemistry and Nanoscience Center, Golden CO 80401
+
+## Installation and Requirements
+To run the dissimilarity_matrix_all.py, Python needs to be installed (quickest way: install Miniconda https://docs.conda.io/en/latest/miniconda.html, and install Spyder by typing `
+conda install -c anaconda spyder
+` on Anaconda Prompt). The following packages also need to be installed.
+
+1.  Pandas (https://pandas.pydata.org/pandas-docs/stable/getting_started/install.html)
+2.  NumPy (https://docs.scipy.org/doc/numpy/user/install.html)
+3.  Seaborn (https://seaborn.pydata.org/installing.html)
+4.  Matplotlib (https://matplotlib.org/users/installing.html)
+5.  SciPy (https://www.scipy.org/install.html)
+6.  Scikit-learn (https://scikit-learn.org/stable/install.html)
+
+OR clone the following repository: `pip install -r requirements.txt`
+
+## Workflow
+The raw image data (.bmp files) needs to be processed first, to extract the average red, green, and blue (RGB) values over time for each samples. The dataset result is shown in DegradationData/(DegradationRun)/RGB/Calibrated folder.
+
+Based on this data, we can calculate the dissimilarity matrix.
+
+In dissimilarity_matrix_all.py, there are different inputs that you can change.
+1. `datapoint`: How many time points you would like to include in the analysis.
+2. `frequency`: How often the degradation images are taken.
+3. `MAPbBrContent_1, MAPbBrContent_2, MAPbBrContent_3`: The amount of MAPbBr<sub>3</sub> of interest.
+4. `concentration_1, concentration_2, concentration_3`: The concentration of capping layers of interest.
+5. `annealing_1, annealing_2, annealing_3`: The annealing temperature for the capping layers of interest.
+6. `capping_2, capping_3`: Capping layer materials of interest.
+7. `metric`: The dissimilarity matrix distance measure, can be 'euclidean', 'cosine', or 'manhattan'.
+8. `folderToSave`: Where to save the analysis results.
+
+Run the dissimilarity_matrix_all.py file.
+
+## Authors
+| |  | 
+|---|---|
+|**Author(s)** | Noor Titan Putri Hartono |
+|**Version** | 1.0/ August 2021  |   
+|**E-mail(s)**   | nhartono at mit dot edu  |
+| | |
+
+## Attribution
+This work is under an Apache 2.0 License. Please, acknowledge use of this work with the appropriate citation to the repository and research article.
+
+## Citation
+
+    @Misc{dissismatrix2021,
+      author =   {The Dissimilarity Matrix authors},
+      title =    {Dissimilarity Matrix for Perovskite Capping-Absorber Pairs},
+      howpublished = {\url{https://github.com/PV-Lab/dissimatrix}},
+      year = {2021}
+    }
---
+++ dissimilarity_matrix_all.py
@@ -0,0 +1,224 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr  6 10:50:59 2021
+
+@author: Noor Titan Hartono
+internal version: 9
+
+Steps in computing dissimilarity matrix (for EACH DATA POINT):
+    1. Load the data, sort them, drop the NA rows.
+    2. Select the data of interest (certain absorbers, certain capping layers
+                                    with certain annealing T and concentrations)
+    3. Calculate the dissimilarity matrix using scikit-learn: pairwise distances.
+    4. Generate figures, and save them in specific path.
+
+"""
+
+# IMPORTING LIBRARIES
+# import scipy.io as sio
+# import numpy.matlib as nm
+import numpy as np
+import matplotlib.pyplot as plt
+# import matplotlib as mpl
+import os
+import pandas as pd
+from collections import OrderedDict
+cmaps = OrderedDict()
+
+
+#%% User inputs
+
+"""
+INPUT FOR THE CODE
+
+datapoint: How many data points you want to include in the analysis, default: 341.
+frequency: How often the degradation images are taken (every... minutes).
+metric: Dissimilarity matrix metric, possible types: 'cosine', 'euclidean',
+                or 'manhattan'.
+                
+MAPbBrContent: Br amount in the absorber (between 0.0-1.0). In this study,
+                the options are: 0, 0.25, 0.5, and 1.0.
+concentration:  Concentration of capping layer precursor.
+annealing: Annealing temperature of capping layer film.
+capping: Capping layer meaterial being analyzed. For 
+folderToSave: Where to save the dissimilarity matrix results, within the 'Results'
+                folder. 
+                
+
+"""
+
+datapoint = 341 #341 # How many data points you want to include in the analysis
+frequency = 3 # How often the degradation images are taken (every .... minutes)
+
+# If we want to compare 3 different data: bare film, PTEAI-based film (10mM, 100C annealing),
+# and 9-Cl-capped film of I : Br = 3 : 1, we can input the following:
+
+# Bare films
+MAPbBrContent_1 = 0.25
+concentration_1 = 0 # Because it's bare film, both concentration and annealing (for capping) is 0s.
+annealing_1 = 0
+
+# PTEAI-capped films
+MAPbBrContent_2 = 0.25
+concentration_2 = 10 # 10 mM
+annealing_2 = 100 # 100C
+capping_2 = "PTEAI"
+
+# 9-Cl-capped films
+MAPbBrContent_3 = 0.25
+concentration_3 = 10 # 10 mM
+annealing_3 = 100 # 100C
+capping_3 = "09"
+
+# Dissimilarity matrix metric: euclidean, manhattan, cosine
+metric = "euclidean" 
+
+folderToSave = "MAPIBr_3_1"
+
+#%% Functions
+
+currentDir = os.path.dirname(os.path.realpath(__file__))
+
+def combineRGB(location, datapoint, frequency):
+    """
+    Combine sliced RGB data according to desired time, with the
+    sample name, capping type, concentration, annealing T.
+    - location: desired degradation folder name
+    - datapoint: how many datapoints you want to include
+    - frequency: the degradation image is taken every ... minutes
+    - RETURN: the giant RGB data, with NaN dropped
+    """
+    R = pd.read_csv(os.path.join(location,'RGB/Calibrated/sample_r_cal.csv'),header=None)
+    G = pd.read_csv(os.path.join(location,'RGB/Calibrated/sample_g_cal.csv'),header=None)
+    B = pd.read_csv(os.path.join(location,'RGB/Calibrated/sample_b_cal.csv'),header=None)
+    samplename = pd.read_csv(os.path.join(location,'RGB/Calibrated/Samples_cap.csv'))
+    
+    # Subtracting data with the initial RGB value of mean(first 11 data points)
+    subtract_datapoint = 11
+    subtract_R = np.expand_dims(np.mean(R.iloc[:,:subtract_datapoint], axis=1),axis=1)
+    subtract_G = np.expand_dims(np.mean(G.iloc[:,:subtract_datapoint], axis=1),axis=1)
+    subtract_B = np.expand_dims(np.mean(B.iloc[:,:subtract_datapoint], axis=1),axis=1)
+    subtract_all = np.expand_dims(np.amin(np.concatenate((subtract_R, subtract_G,
+                                        subtract_B), axis=1), axis=1),axis=1)
+    
+    # Combining RGB 
+    RGB = pd.concat([R.iloc[:,:datapoint],
+                     G.iloc[:,:datapoint],
+                     B.iloc[:,:datapoint]],axis=1)
+    
+    RGB_sub = RGB.subtract(subtract_all, axis=1)
+    
+    # Combine sample name with RGB, drop rows with NaN values
+    RGB_samplename = (pd.concat([samplename, RGB_sub], axis=1)).dropna(axis=0)
+    
+    # Sort by Capping column, then concentration, if preferred
+    # RGB_samplename_sorted = RGB_samplename.sort_values(by=['Capping', 'Concentration', 'Annealing'])
+    
+    return (RGB_samplename)
+
+#%% Generating dissimilarity matrix
+
+# Looping for the time range of interest, calculating for dissimilarity matrix
+# at each time range
+
+for i in range(datapoint):
+    
+    # LOADING RGB DATA
+    os.chdir(currentDir+'/DegradationData/')
+    
+    # LOADING RGB DATA, 100% MAPI using July samples
+    MAPIBr_1_0_1to5 = combineRGB('20200714-R1-TH',i+1,frequency)
+    MAPIBr_1_0_6to10 = combineRGB('20200715-R1-TH',i+1,frequency)
+    MAPIBr_1_0_2nd = combineRGB('20201001-R1-JT',i+1,frequency)
+    
+    MAPIBr_3_1_1to5 = combineRGB('20200710-R1-JT',i+1,frequency)
+    MAPIBr_3_1_6to10 = combineRGB('20200720-R1-TH',i+1,frequency)
+    MAPIBr_3_1_2nd = combineRGB('20201005-R1-TH',i+1,frequency)
+    
+    MAPIBr_1_1_1to5 = combineRGB('20200707-R1-JT4TH',i+1,frequency)
+    MAPIBr_1_1_6to10 = combineRGB('20200721-R1-JT',i+1,frequency)
+    MAPIBr_1_1_2nd = combineRGB('20201006-R1-TH',i+1,frequency)
+    
+    MAPIBr_1_3_1to5 = combineRGB('20200724-R1-JT',i+1,frequency)
+    MAPIBr_1_3_6to10 = combineRGB('20200812-R1-TH',i+1,frequency)
+    MAPIBr_1_3_2nd = combineRGB('20201008-R1-TH',i+1,frequency)
+    
+    MAPIBr_0_1_1to5 = combineRGB('20200730-R1-TH',i+1,frequency)
+    MAPIBr_0_1_6to10 = combineRGB('20200814-R1-TH',i+1,frequency)
+    MAPIBr_0_1_2nd = combineRGB('20201013-R1-JT',i+1,frequency)
+    
+    # CONCATENATING all
+
+    deg_all_samplename = pd.concat([MAPIBr_1_0_1to5, MAPIBr_1_0_6to10, MAPIBr_1_0_2nd,
+                                    MAPIBr_3_1_1to5, MAPIBr_3_1_6to10, MAPIBr_3_1_2nd,
+                                    MAPIBr_1_1_1to5, MAPIBr_1_1_6to10, MAPIBr_1_3_1to5, 
+                                    MAPIBr_1_1_2nd, MAPIBr_1_3_6to10, MAPIBr_1_3_2nd,
+                                    MAPIBr_0_1_1to5, MAPIBr_0_1_6to10, MAPIBr_0_1_2nd],
+                                    axis=0)
+    
+    # SORTING based on absorbers first OR capping first
+    
+    # Sorting based on absorbers first
+    sorted_absorber = deg_all_samplename.sort_values(by=['MAPbBr', 'Capping', 
+                                                     'Concentration', 'Annealing'])
+    
+    # Sorting based on capping layer first
+    sorted_capping = deg_all_samplename.sort_values(by=['Capping', 'MAPbBr', 
+                                                     'Concentration', 'Annealing'])
+    
+    # DROPPING based on certain values
+
+    sorted_absorber_1 = sorted_absorber[((sorted_absorber['MAPbBr'] == MAPbBrContent_1) &
+                                          (sorted_absorber['Concentration'] == concentration_1) &
+                                          (sorted_absorber['Annealing'] == annealing_1))]
+    
+    sorted_absorber_2 = sorted_absorber[((sorted_absorber['MAPbBr'] == MAPbBrContent_2) &
+                                        (sorted_absorber['Concentration'] == concentration_2) &
+                                        (sorted_absorber['Annealing'] == annealing_2) &
+                                        (sorted_absorber['Capping'] == capping_2))]
+    
+    sorted_absorber_3 = sorted_absorber[((sorted_absorber['MAPbBr'] == MAPbBrContent_3) &
+                                        (sorted_absorber['Concentration'] == concentration_3) &
+                                        (sorted_absorber['Annealing'] == annealing_3) &
+                                        (sorted_absorber['Capping'] == capping_3))]
+    
+    # COMBINING all the sorted_absorber data
+                                            
+    sorted_absorber_10mM_100C = pd.concat([sorted_absorber_1, sorted_absorber_2,
+                                          sorted_absorber_3], axis=0)
+                                            
+    
+    # CALCULATING the dissimilarity matrix
+    from sklearn.metrics.pairwise import pairwise_distances
+
+    d_absorber = pairwise_distances(sorted_absorber_10mM_100C.drop(sorted_absorber_10mM_100C.iloc[:, 0:9], 
+                                           axis = 1), metric=metric) # The first 9 being dropped because they're not actual data
+
+    
+    # VISUALIZING dissimilarity matrix
+    
+    # Where to save the 
+    os.chdir(currentDir+'/Results/'+folderToSave+'/bare_cap9_PTEAI/')
+   
+    plt.figure(i+1,figsize=(8,6),dpi=300) # for all use 10,10; 8,6 for subsets
+    plt.rcParams['font.family'] = 'Arial'
+    plt.rcParams['font.size'] = 24 # for all: 20, 24 for subsets
+    plt.clf()
+    plt.imshow(d_absorber)
+    
+    plt.xlabel('Samples')
+    plt.ylabel('Samples')
+    
+    # For any Br-mixed films
+    plt.xticks(np.array([5.5,13,16.5]),('Bare','Cap9','PTEAI'))
+    plt.yticks(np.array([5.5,13,16.5]),('Bare','Cap9','PTEAI'))
+    
+    # For just I (0 Br) films
+    # plt.xticks(np.array([5,12,15.5]),('Bare','Cap9','PTEAI'))
+    # plt.yticks(np.array([5,12,15.5]),('Bare','Cap9','PTEAI'))
+    
+    plt.clim(0,1e3) # Adjust colorbar range
+    # plt.xlim([-0.5,9.5]) # Adjust xlim 
+    plt.colorbar()
+    plt.savefig('barecap9PTEAI_'+str(f'{i:03}')+'.png') # How to save the files
+
